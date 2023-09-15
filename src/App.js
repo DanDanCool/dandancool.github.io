f87d513f93f9db2a32961ea7add562ac7dca2498
Daniel Sun <sun.daniel@outlook.com>
2023-09-15 17:24:17 +0000

    add images

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 	  <Route path="/" element={<Layout />} >
 	  <Route index element={<Home />} />
 	  <Route path="about" element={<About />} />
+	  <Route path="blog" element={<Blog />} />
 	  <Route path="*" element={<NotFound />} />
 	  </Route>
 	</Routes>
@@ -30,8 +31,8 @@ function Layout() {
 function Home() {
 	return (
     <div className="">
-      <header className="text-4xl p-5 text-center">
-          Recent Posts
+      <header className="text-4xl p-5 text-center tracking-wide">
+          <Link to="/blog">Recent Posts</Link>
       </header>
 	  <BlogPosts/>
     </div>
@@ -41,7 +42,50 @@ function Home() {
 function About() {
 	return (
 		<div>
-		About Me
+		<div className="p-5 text-4xl uppercase tracking-widest first-letter:text-6xl first-letter:font-bold font-serif text-center">
+		Simplicity is elegance
+		</div>
+		<div className="p-10 text-2xl text-left">
+		<img class="pr-10 float-left object-contain h-80 w-80" src="woomy2_sq.png"/>
+		<p className="text-2xl text-left mb-10">
+		This is my programming philosophy. Simple solutions and systems are easier to work with and reason about, improving maintainability and reducing bugs.
+		This site embraces simplicity, I definitely did not decide writing css was too much work! Regardless, I think simplicity is pretty great, don't you agree?
+		</p>
+		<img class="pl-10 float-right object-contain h-80 w-80" src="woomy4_sq.png"/>
+		<p className="text-2xl text-left mb-10">
+		My name is Daniel, I am an engineering student at the University of Waterloo. I am looking for work! If you are interested in contacting me please reach out via
+		email (you can access that by clicking the contact button in the navigation bar). I primarily create tools for C and C++ to streamline and simplify the development experience.
+		My latest work:&nbsp;
+		<a href="https://pypi.org/project/jmake/" className="underline">jmake</a>
+		,&nbsp;takes advantage of the simplicity of python to replace a hodgepodge of shell scripts and CMake found in most C and C++ projects.
+		</p>
+		<p className="text-2xl text-left mb-10">
+		Until recently, I almost exclusively programmed in C and C++, I love the low level control - it's familiar and comfortable. That all changed when I entered the&nbsp;
+		<a href="https://uwaterloo.ca/centre-for-work-integrated-learning/waterloo-experience-we-accelerate" className="underline">WE Accelerate</a>
+		&nbsp;program, where I was exposed to new technologies - Azure Cloud and Azure AI. During my time there, I designed and deployed a&nbsp;
+		<a href="https://gist.github.com/DanDanCool/151bbdf83e4c3872e28a6255c66dd6b5" className="underline">chatbot assistant</a>
+		&nbsp;on Azure Cloud using their newly added AI services. Through this experience I found that I really enjoyed AI! The next thing I accomplished was to create a basic detection&nbsp;
+		<a href="https://github.com/DanDanCool/seer" className="underline">computer vision model</a>
+		. It leverages Unreal Engine to automatically generate training data, removing the need for humans to manually define a bounding box.
+		</p>
+		<p className="text-2xl text-left mb-10">
+		Still, C and C++ holds a special place in my heart. So of course my next project was written in C. This time I wanted to design logic that scales well with multiple threads.
+		I adapted my preexisting code and separated them out into pure functions. This is useful as it prevents false-sharing and removes the need for synchronization. This easily allows
+		for arbitrary scaling using a thread pool. For more details click&nbsp;
+		<a href="https://github.com/DanDanCool/fuzzy" className="underline">here</a>. In my experience, writing C with stdlib can be very cumbersome. I have written a support library with
+		common data structures to rememdy this problem. It emulates generics using macros so it can be messy but I think it's better than copying and pasting around a bunch of code. Check
+		out the library&nbsp;
+		<a href="https://github.com/DanDanCool/jollyc" className="underline">here</a>.
+		</p>
+		</div>
+		</div>
+	);
+}
+
+function Blog() {
+	return (
+		<div>
+		blog
 		</div>
 	);
 }
@@ -53,3 +97,4 @@ function NotFound() {
 		</div>
 	);
 }
+
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,18 +1,18 @@
 import {Link} from 'react-router-dom'
 function NavBar() {
 	return (
-	<div className="flex flex-row items-center justify-between text-2xl">
-	  <Link to="/" className="px-5">Daniel's Blog</Link>
+	<div className="flex flex-row items-center justify-between text-2xl tracking-wide">
+	  <Link to="/" className="px-10">Daniel's Blog</Link>
 	  <nav>
 		<ul class = "flex flex-row">
 		  <li>
-		  <Link to="/about" className="p-5">about</Link>
+		  <Link to="/about" className="p-10">about</Link>
 		  </li>
 		  <li>
-		  <a href="mailto:sun.daniel@outlook.com" className="p-5">contact</a>
+		  <a href="mailto:sun.daniel@outlook.com" className="p-10">contact</a>
 		  </li>
 		  <li>
-		  <a href="https://github.com/DanDanCool" className="p-5">github</a>
+		  <a href="https://github.com/DanDanCool" className="p-10">github</a>
 		  </li>
 		</ul>
 	  </nav>
